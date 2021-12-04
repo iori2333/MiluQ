a39506b61cdb1a5575d8380b19f966d27ad7a6d0
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-12-04 15:38:38 +0800

    chore: update project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import visualizer from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          'react': ['react', 'react-dom', 'react-router-dom'],
-          'utils': ['dayjs', 'socket.io-client']
+          react: [
+            'react',
+            'react-redux',
+            'redux',
+            '@reduxjs/toolkit',
+            'react-dom',
+            'react-router-dom'
+          ],
+          utils: ['dayjs', 'socket.io-client']
         }
       }
     }
