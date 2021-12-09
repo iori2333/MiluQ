605dab3a5e9cb5c296ffb20050d08e08658f9b1a
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-12-09 21:46:22 +0800

    chore: fix eslint code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,6 @@ module.exports = {
   },
   plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
-    indent: ['warn', 2],
-    quotes: ['warn', 'single'],
-    semi: ['warn', 'always'],
     '@typescript-eslint/no-empty-function': 'off',
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
